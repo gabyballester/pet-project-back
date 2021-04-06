ebd7425e74ef1526c378d7eb5e4cf10a844baf50
Gabriel Ballester <gaby.ballester@gmail.com>
2021-04-06 13:04:39 +0200

    Configura CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var cors = require('cors');
 
 const app = express();
 const { API_VERSION } = require("./config");
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Configure Header HTTP (CORS)
+app.use(cors());
 
 // Router Basic
 app.use(`/api/${API_VERSION}/user`, userRoutes);
